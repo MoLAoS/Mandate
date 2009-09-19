042dfccf1fbed5ff3822cfabac5f8f86c7ff881d
daniel_santos <daniel_santos@users.sourceforge.net>
2009-09-19 07:53:23 +0000

    This is a partial (mostly complete) merge from the network branch.  For the most part, it simply cleans up code formatting (whitespace) and moves everything from the Glest::MapEditor namespace to MapEditor. * Removed .orig files (which don't need to be in here) * Renamed "players" to "factions" everywhere because of the concept (being   introduced in the network branch) that a single player may control more than   one faction. * Added appropriate headers to all files (with copyrights, license, etc.) and   added pch support. * Made appropriate changes to use new Shared::Util::Conversion class instead of   global functions.

--- source/glest_map_editor/program.cpp
+++ source/glest_map_editor/program.cpp
@@ -1,132 +1,144 @@
+// ==============================================================
+//	This file is part of Glest (www.glest.org)
+//
+//	Copyright (C) 2001-2008 Martiño Figueroa
+//
+//	You can redistribute this code and/or modify it under
+//	the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the
+//	License, or (at your option) any later version
+// ==============================================================
+
+#include "pch.h"
 #include "program.h"
 
-#include "util.h" 
+#include "util.h"
 
 using namespace Shared::Util;
 
-namespace Glest{ namespace MapEditor{
+namespace MapEditor {
 
 // ===============================================
 //	class Program
 // ===============================================
 
-Program::Program(int w, int h){
-     cellSize=6;
-     ofsetX= 0;
-     ofsetY= 0;
-     map= new Map();
-     renderer.init(w, h);
+Program::Program(int w, int h) {
+	cellSize = 6;
+	ofsetX = 0;
+	ofsetY = 0;
+	map = new Map();
+	renderer.init(w, h);
 }
 
-Program::~Program(){
-     delete map;
+Program::~Program() {
+	delete map;
 }
 
-void Program::changeMapHeight(int x, int y, int Height, int radius){
-     map->changeHeight((x-ofsetX)/cellSize, (y+ofsetY)/cellSize, Height, radius);
+void Program::changeMapHeight(int x, int y, int Height, int radius) {
+	map->changeHeight((x - ofsetX) / cellSize, (y + ofsetY) / cellSize, Height, radius);
 }
 
-void Program::changeMapSurface(int x, int y, int surface, int radius){
-     map->changeSurface((x-ofsetX)/cellSize, (y+ofsetY)/cellSize, surface, radius);
+void Program::changeMapSurface(int x, int y, int surface, int radius) {
+	map->changeSurface((x - ofsetX) / cellSize, (y + ofsetY) / cellSize, surface, radius);
 }
 
-void Program::changeMapObject(int x, int y, int object, int radius){
-     map->changeObject((x-ofsetX)/cellSize, (y+ofsetY)/cellSize, object, radius);
+void Program::changeMapObject(int x, int y, int object, int radius) {
+	map->changeObject((x - ofsetX) / cellSize, (y + ofsetY) / cellSize, object, radius);
 }
 
-void Program::changeMapResource(int x, int y, int resource, int radius){
-     map->changeResource((x-ofsetX)/cellSize, (y+ofsetY)/cellSize, resource, radius);
+void Program::changeMapResource(int x, int y, int resource, int radius) {
+	map->changeResource((x - ofsetX) / cellSize, (y + ofsetY) / cellSize, resource, radius);
 }
 
-void Program::changeStartLocation(int x, int y, int player){    
-     map->changeStartLocation((x-ofsetX)/cellSize, (y+ofsetY)/cellSize, player);
+void Program::changeStartLocation(int x, int y, int player) {
+	map->changeStartLocation((x - ofsetX) / cellSize, (y + ofsetY) / cellSize, player);
 }
 
-void Program::renderMap(int w, int h){
-     renderer.renderMap(map, ofsetX, ofsetY, w, h, cellSize);
+void Program::renderMap(int w, int h) {
+	renderer.renderMap(map, ofsetX, ofsetY, w, h, cellSize);
 }
 
-void Program::setRefAlt(int x, int y){
-	map->setRefAlt((x-ofsetX)/cellSize, (y+ofsetY)/cellSize);
+void Program::setRefAlt(int x, int y) {
+	map->setRefAlt((x - ofsetX) / cellSize, (y + ofsetY) / cellSize);
 }
 
-void Program::flipX(){
+void Program::flipX() {
 	map->flipX();
 }
 
-void Program::flipY(){
+void Program::flipY() {
 	map->flipY();
 }
 
-void Program::randomizeMapHeights(){
+void Program::randomizeMapHeights() {
 	map->randomizeHeights();
 }
 
-void Program::randomizeMap(){
+void Program::randomizeMap() {
 	map->randomize();
 }
 
-void Program::switchMapSurfaces(int surf1, int surf2){
+void Program::switchMapSurfaces(int surf1, int surf2) {
 	map->switchSurfaces(surf1, surf2);
 }
 
-void Program::reset(int w, int h, int alt, int surf){
-     map->reset(w, h, (float) alt, surf);
+void Program::reset(int w, int h, int alt, int surf) {
+	map->reset(w, h, (float) alt, surf);
 }
 
-void Program::resize(int w, int h, int alt, int surf){
-     map->resize(w, h, (float) alt, surf);
+void Program::resize(int w, int h, int alt, int surf) {
+	map->resize(w, h, (float) alt, surf);
 }
 
-void Program::resetPlayers(int maxPlayers){
-     map->resetPlayers(maxPlayers);
+void Program::resetFactions(int maxFactions) {
+	map->resetFactions(maxFactions);
 }
 
-void Program::setMapTitle(const string &title){
-     map->setTitle(title);
-}
- 
-void Program::setMapDesc(const string &desc){
-     map->setDesc(desc);
+void Program::setMapTitle(const string &title) {
+	map->setTitle(title);
 }
 
-void Program::setMapAuthor(const string &author){
-     map->setAuthor(author);
+void Program::setMapDesc(const string &desc) {
+	map->setDesc(desc);
 }
 
-void Program::setOfset(int x, int y){
-     ofsetX+= x;
-     ofsetY-= y;
+void Program::setMapAuthor(const string &author) {
+	map->setAuthor(author);
 }
 
-void Program::incCellSize(int i){
-    
-	int minInc= 2-cellSize;
+void Program::setOfset(int x, int y) {
+	ofsetX += x;
+	ofsetY -= y;
+}
 
-	if(i<minInc){
-		i= minInc;
+void Program::incCellSize(int i) {
+
+	int minInc = 2 - cellSize;
+
+	if (i < minInc) {
+		i = minInc;
 	}
-	cellSize+= i;
-	ofsetX-= (map->getW()*i)/2;
-	ofsetY+= (map->getH()*i)/2;
+	cellSize += i;
+	ofsetX -= (map->getW() * i) / 2;
+	ofsetY += (map->getH() * i) / 2;
 }
 
-void Program::resetOfset(){
-     ofsetX= 0;
-     ofsetY= 0;
-     cellSize= 6;
+void Program::resetOfset() {
+	ofsetX = 0;
+	ofsetY = 0;
+	cellSize = 6;
 }
 
-void Program::setMapAdvanced(int altFactor, int waterLevel){
-     map->setAdvanced(altFactor, waterLevel);
+void Program::setMapAdvanced(int altFactor, int waterLevel) {
+	map->setAdvanced(altFactor, waterLevel);
 }
 
-void Program::loadMap(const string &path){
-     map->loadFromFile(path);
+void Program::loadMap(const string &path) {
+	map->loadFromFile(path);
 }
 
-void Program::saveMap(const string &path){
-     map->saveToFile(path);
+void Program::saveMap(const string &path) {
+	map->saveToFile(path);
 }
 
-}}// end namespace
+}// end namespace
--- source/glest_map_editor/program.h
+++ source/glest_map_editor/program.h
@@ -1,63 +1,74 @@
-#ifndef _GLEST_MAPEDITOR_PROGRAM_H_
-#define _GLEST_MAPEDITOR_PROGRAM_H_
+// ==============================================================
+//	This file is part of Glest (www.glest.org)
+//
+//	Copyright (C) 2001-2008 Martiño Figueroa
+//
+//	You can redistribute this code and/or modify it under
+//	the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the
+//	License, or (at your option) any later version
+// ==============================================================
+
+#ifndef _MAPEDITOR_PROGRAM_H_
+#define _MAPEDITOR_PROGRAM_H_
 
 #include "map.h"
 #include "renderer.h"
 
-namespace Glest{ namespace MapEditor{
+namespace MapEditor {
 
 class MainWindow;
 
 // ===============================================
-//	class Program
+// class Program
 // ===============================================
 
-class Program{
+class Program {
 private:
 	Renderer renderer;
-    int ofsetX, ofsetY;
-    int cellSize;
-    Map *map;
+	int ofsetX, ofsetY;
+	int cellSize;
+	Map *map;
 
 public:
-    Program(int w, int h);
-    ~Program();
+	Program(int w, int h);
+	~Program();
 
 	//map cell change
 	void changeMapHeight(int x, int y, int Height, int radius);
-    void changeMapSurface(int x, int y, int surface, int radius);
-    void changeMapObject(int x, int y, int object, int radius);
-    void changeMapResource(int x, int y, int resource, int radius);
-    void changeStartLocation(int x, int y, int player);
+	void changeMapSurface(int x, int y, int surface, int radius);
+	void changeMapObject(int x, int y, int object, int radius);
+	void changeMapResource(int x, int y, int resource, int radius);
+	void changeStartLocation(int x, int y, int player);
 
 	//map ops
-    void reset(int w, int h, int alt, int surf);
-    void resize(int w, int h, int alt, int surf);
-    void resetPlayers(int maxPlayers);
+	void reset(int w, int h, int alt, int surf);
+	void resize(int w, int h, int alt, int surf);
+	void resetFactions(int maxFactions);
 	void setRefAlt(int x, int y);
 	void flipX();
 	void flipY();
 	void randomizeMapHeights();
 	void randomizeMap();
 	void switchMapSurfaces(int surf1, int surf2);
-    void loadMap(const string &path);
-    void saveMap(const string &path);
+	void loadMap(const string &path);
+	void saveMap(const string &path);
 
 	//map misc
 	void setMapTitle(const string &title);
-    void setMapDesc(const string &desc);
-    void setMapAuthor(const string &author);
-    void setMapAdvanced(int altFactor, int waterLevel);
+	void setMapDesc(const string &desc);
+	void setMapAuthor(const string &author);
+	void setMapAdvanced(int altFactor, int waterLevel);
 
 	//misc
 	void renderMap(int w, int h);
 	void setOfset(int x, int y);
-    void incCellSize(int i);
-    void resetOfset();
+	void incCellSize(int i);
+	void resetOfset();
 
 	const Map *getMap() {return map;}
 };
 
-}}// end namespace
+}// end namespace
 
 #endif
