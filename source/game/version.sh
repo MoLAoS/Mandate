3a95bca603b81bd0206a9fc0f5ce0e5aff9adf60
silnarm <silnarm@gmail.com>
2011-05-31 08:36:08 +1000

    Fix cmake add_custom_command for version.sh (I think...)

--- source/game/version.sh
+++ source/game/version.sh
@@ -3,6 +3,6 @@
 date_string=`date`
 sha_string=`git describe`
 
-echo \#include "version.h" > version.c
-echo "const char * build_date = \"$date_string\";" >> version.c
-echo "const char * build_git_sha = \"$sha_string\";" >> version.c
+echo \#include \"version.h\" > version.cpp
+echo "const char * build_date = \"$date_string\";" >> version.cpp
+echo "const char * build_git_sha = \"$sha_string\";" >> version.cpp
