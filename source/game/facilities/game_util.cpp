652520c0bf03b0d93131b30628c385b529fbc575
silnarm <silnarm@gmail.com>
2011-06-21 02:37:52 +1000

    rejig debug ed version string (back to 'de', now with git hash)

--- source/game/facilities/game_util.cpp
+++ source/game/facilities/game_util.cpp
@@ -31,12 +31,8 @@ const string gaeMailString= CONTACT_STRING;
 
 const string glestVersionString= "v3.2.2";
 
-#if _GAE_DEBUG_EDITION_ && MAD_SYNC_CHECKING
-#	error MAD_SYNC_CHECKING and _GAE_DEBUG_EDITION_ not a good idea
-#elif _GAE_DEBUG_EDITION_
-	const string gaeVersionString = string(VERSION_STRING) + "_dev_edition";
-#elif MAD_SYNC_CHECKING
-	const string gaeVersionString = string(VERSION_STRING) + "_sync_test";
+#if _GAE_DEBUG_EDITION_
+	const string gaeVersionString = string(VERSION_STRING) + "_de (" + getGitHash() + ")";
 #else
 	const string gaeVersionString = string(VERSION_STRING) + " (" + getGitHash() + ")";
 #endif
