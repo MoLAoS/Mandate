3118a87cb6b273184c325e6bc58e16aaf1ed3b0f
bloodwurm <bloodwurm@gmail.com>
2011-12-15 21:38:19 -0500

    forgot those files, part of the music playlist feature
    Signed-off-by: hailstone3 <hailstone3@users.sourceforge.net>

---
+++ source/game/prototypes/music_playlist_type.cpp
@@ -0,0 +1,148 @@
+// ==============================================================
+//	This file is part of Glest Advanced Engine
+//
+//	GPL V2. see source/liscence.txt
+// ==============================================================
+
+#include "pch.h"
+#include "music_playlist_type.h"
+
+#include "xml_parser.h"
+#include "sound.h"
+#include "leak_dumper.h"
+
+using namespace Shared::Xml;
+using namespace Shared::Sound;
+
+
+namespace Glest { namespace ProtoTypes {
+
+// =====================================================
+// 	class UpgradeType
+// =====================================================
+
+// ==================== misc ====================
+
+MusicPlaylistType::MusicPlaylistType() 
+    : m_bRandom(false)
+    , m_bLooping(false)
+    , m_activationType(eActivationType_AddToCurrent)
+    , m_activation(eActivationMode_Wait)
+    , m_curTrackIndex(-1) {
+}
+
+MusicPlaylistType::~MusicPlaylistType() {
+    for(int i= 0; i < m_tracks.size(); i++)
+        delete m_tracks[i];
+}
+
+void MusicPlaylistType::preload( const XmlNode *pXMLNode, const string &dir ) {
+    const XmlAttribute *randomAttrib = pXMLNode->getAttribute("randomize", false);
+    m_bRandom = (randomAttrib && randomAttrib->getBoolValue() ? true : false);
+
+    const XmlAttribute *loopingAttrib = pXMLNode->getAttribute("loop", false);
+    m_bLooping = (loopingAttrib && loopingAttrib->getBoolValue() ? true : false);
+
+    const XmlAttribute *actTypeAttrib = pXMLNode->getAttribute("activation-type", false);
+    if(actTypeAttrib) {
+        if(strcmp(actTypeAttrib->getRestrictedValue().c_str(), "add")== 0) {
+            m_activationType= eActivationType_AddToCurrent;
+        }
+        else if(strcmp(actTypeAttrib->getRestrictedValue().c_str(), "replace")== 0) {
+            m_activationType= eActivationType_ReplaceCurrent;
+        }
+        else {
+		    throw runtime_error("Unsupported value specified for activation-type!");
+        }
+    }
+
+    const XmlAttribute *actModeAttrib = pXMLNode->getAttribute("activation-mode", false);
+    if(actModeAttrib) {
+        if(strcmp(actModeAttrib->getRestrictedValue().c_str(), "wait")== 0) {
+            m_activation= eActivationMode_Wait;
+        }
+        else if(strcmp(actModeAttrib->getRestrictedValue().c_str(), "interrupt")== 0) {
+            m_activation= eActivationMode_Interrupt;
+        }
+        else {
+		    throw runtime_error("Unsupported value specified for activation-mode!");
+        }
+    }
+
+    for (int i=0; i < pXMLNode->getChildCount(); ++i) {
+	    StrSound *sound = new StrSound();
+	    sound->open(dir + "/" + pXMLNode->getChild("music-file", i)->getAttribute("path")->getRestrictedValue());
+	    addTrack(sound);
+    }
+	if (m_tracks.empty()) {
+		throw runtime_error("No tracks in play-list!");
+	}
+}
+
+StrSound* MusicPlaylistType::getNextTrack() { 
+    if(m_curTrackIndex == -1) {
+        // playlist is starting up
+        m_curTrackIndex = 0;
+        if(m_bRandom) {
+		    int seed = int(Chrono::getCurTicks());
+		    Random random(seed);
+		    m_curTrackIndex = random.randRange(0, m_tracks.size() - 1);
+        }
+    } 
+    else if(!m_bRandom) {
+        // playlist in sequential order
+        m_curTrackIndex++;
+        if(m_curTrackIndex == m_tracks.size()) {
+            if(m_bLooping) {
+                m_curTrackIndex = 0;
+            }
+            else {
+                m_curTrackIndex = -1;
+            }
+        }
+    } 
+    else {
+       // random playlist
+        if(!m_bLooping) {
+            m_curTrackIndex = -1;
+        }
+        else if(m_tracks.size() == 2) {
+            m_curTrackIndex = !m_curTrackIndex;
+        } 
+        else {
+            int seed = int(Chrono::getCurTicks());
+	        Random random(seed);
+
+            int rndIndex = m_curTrackIndex;
+            while(rndIndex == m_curTrackIndex) {
+		        rndIndex = random.randRange(0, m_tracks.size() - 1);
+            }
+            m_curTrackIndex = rndIndex;
+        }
+    }
+
+    return (m_curTrackIndex >= 0) ? m_tracks[m_curTrackIndex] : 0; 
+}
+
+
+UnitMusicPlaylistType::UnitMusicPlaylistType() 
+    : MusicPlaylistType()
+    , m_activationTime(eActivationTime_UnitCreated) {
+}
+
+void UnitMusicPlaylistType::preload( const XmlNode *pXMLNode, const string &dir )
+{
+    MusicPlaylistType::preload(pXMLNode, dir);
+
+    const XmlAttribute *actTimeAttrib = pXMLNode->getAttribute("activation-time", false);
+    if(actTimeAttrib) {
+        if(strcmp(actTimeAttrib->getRestrictedValue().c_str(), "build")== 0) {
+            m_activationTime= eActivationTime_UnitBuildStarted;
+        }
+        else if(strcmp(actTimeAttrib->getRestrictedValue().c_str(), "created")== 0) {
+            m_activationTime= eActivationTime_UnitCreated;
+        }
+    }
+}
+
+}}//end namespace
---
+++ source/game/prototypes/music_playlist_type.h
@@ -0,0 +1,109 @@
+// ==============================================================
+//	This file is part of Glest Advanced Engine
+//
+//	GPL V2. see source/liscence.txt
+// ==============================================================
+
+#ifndef _GLEST_GAME_MUSICPLAYLIST_H_
+#define _GLEST_GAME_MUSICPLAYLIST_H_
+
+
+namespace Shared { 
+    namespace Xml {
+        class XmlNode;
+    }
+
+    namespace Sound {
+        class StrSound; 
+    }
+}
+
+using namespace Shared::Xml;
+using Shared::Sound::StrSound;
+
+
+namespace Glest { namespace ProtoTypes {
+
+// ===============================
+// 	class MusicPlaylistType
+// ===============================
+
+/** A list of music files to play when in a game. Music files can be played in sequence or randomly. */
+class MusicPlaylistType {
+public:
+
+    enum eActivationType {
+        eActivationType_AddToCurrent= 0,
+        eActivationType_ReplaceCurrent
+    };
+
+    enum eActivationMode {
+        eActivationMode_Wait= 0,
+        eActivationMode_Interrupt,
+    };
+
+	typedef vector<StrSound*> MusicTracks;
+
+private:
+	MusicTracks         m_tracks;
+    bool                m_bRandom;
+    bool                m_bLooping;
+    eActivationType     m_activationType;
+    eActivationMode     m_activation;
+
+    int                 m_curTrackIndex;
+
+public:
+	MusicPlaylistType();
+	~MusicPlaylistType();
+
+    virtual void preload( const XmlNode *pXMLNode, const string &dir );
+
+    void setRandom(bool random) { m_bRandom= random; }
+    bool isRandom() const { return m_bRandom; }
+
+    void setLoop(bool loop) { m_bLooping= loop; }
+    bool isLooping() const { return m_bLooping; }
+
+    void setActivationType(eActivationType actType) { m_activationType= actType; }
+    eActivationType getActivationType() const { return m_activationType; }
+
+    void setActivationMode(eActivationMode actMode) { m_activation= actMode; }
+    eActivationMode getActivationMode() const { return m_activation; }
+
+    MusicTracks& getTrackList() { return m_tracks; }
+    void addTrack(StrSound* newTrack) { m_tracks.push_back(newTrack); }
+    bool empty() const { return m_tracks.empty(); }
+    StrSound* getNextTrack();
+};
+
+// ===============================
+// 	class UnitMusicPlaylistType
+// ===============================
+
+/** A list of music files to play related to a unit. Music files can be played in sequence or randomly. 
+    I still strongly believe this shouldn't be done in the engine but handled through the scripting
+    solution (lua or other, when available) **/
+class UnitMusicPlaylistType: public MusicPlaylistType {
+public:
+
+    enum eActivationTime {
+        eActivationTime_UnitBuildStarted= 0,
+        eActivationTime_UnitCreated
+    };
+
+private:
+    eActivationTime     m_activationTime;
+
+public:
+	UnitMusicPlaylistType();
+
+    virtual void preload( const XmlNode *pXMLNode, const string &dir );
+
+    void setActivationTime(eActivationTime actTime) { m_activationTime= actTime; }
+    eActivationTime getActivationTime() const { return m_activationTime; }
+};
+
+}} // namespace Glest::ProtoTypes
+
+#endif
